c6b93ae9a312ad614e1a954a3f302a5bac579e0b
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2026-03-12 15:49:17 -0700

    Import Blackboard

---
+++ Assets/1-QuantumCatastrophe/Scripts/Utilities/Blackboard/Arbiter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QC.Utilities.BlackboardSystem
+{
+    public class Arbiter
+    {
+        private readonly List<IExpert> m_experts = new();
+        
+        public List<IExpert> GetExperts => m_experts;
+        
+        public void RegisterExpert(IExpert expert)
+        {
+            if (expert == null) return;
+            m_experts.Add(expert);
+        }
+        
+        public void DeregisterExpert(IExpert expert)
+        {
+            if (expert == null) return;
+            if (!m_experts.Contains(expert)) return;
+            m_experts.Remove(expert);
+        }
+
+        public List<Action> BlackboardIteration(Blackboard blackboard)
+        {
+            IExpert bestExpert = null;
+            int highestInsistence = 0;
+
+            foreach (IExpert expert in m_experts)
+            {
+                int insistence = expert.GetInsistence(blackboard);
+                if (insistence > highestInsistence)
+                {
+                    highestInsistence = insistence;
+                    bestExpert = expert;
+                }
+            }
+            
+            bestExpert?.Execute(blackboard);
+            
+            List<Action> actions = new(blackboard.PassedActions);
+            blackboard.ClearActions();
+            
+            return actions;
+        }
+    }
+}
