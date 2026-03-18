f703068964f1721cae84b49c54ecd3e521da23e6
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2026-03-17 18:30:41 -0700

    Reset double jump after dashing through barrier

--- Assets/1-QuantumCatastrophe/Scripts/Environment/Obstacles/EnergyBarrier.cs
+++ Assets/1-QuantumCatastrophe/Scripts/Environment/Obstacles/EnergyBarrier.cs
@@ -7,6 +7,30 @@ public class EnergyBarrier : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        AddForceInDashDirection(other);
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        ResetDashAndJump(other);
+    }
+
+    private void ResetDashAndJump(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        if (player)
+        {
+            m_collider.SetActive(true);
+            CharacterAbilities abilities = player.GetComponent<CharacterAbilities>();
+            CharacterMovement2D movement = player.GetComponent<CharacterMovement2D>();
+            movement.ResetJumpCount();
+            abilities.RechargeDashCooldown();
+            abilities.RechargeAirDashCooldown();
+        }
+    }
+
+    private void AddForceInDashDirection(Collider2D other)
+    {
         PlayerController player = other.gameObject.GetComponent<PlayerController>();
         if (player)
         {
@@ -18,19 +42,6 @@ public class EnergyBarrier : MonoBehaviour
                 m_collider.SetActive(false);
                 rb.AddForce(abilities.DashDirection * 50f, ForceMode2D.Impulse);
             }
-            
-        }
-    }
-    
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        PlayerController player = other.gameObject.GetComponent<PlayerController>();
-        if (player)
-        {
-            m_collider.SetActive(true);
-            CharacterAbilities abilities = player.GetComponent<CharacterAbilities>();
-            abilities.RechargeDashCooldown();
-            abilities.RechargeAirDashCooldown();
         }
     }
 }
